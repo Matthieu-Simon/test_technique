9b8a152e79edba55c17b3dd699445182b244f95c
Matthieu-Simon <simon.matthieu89@gmail.com>
2025-07-10 23:20:08 +0200

    Création des composants Title & Button pour la section Header

---
+++ packages/ui/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Title from "./Title";
+import Button from "./Button";
+
+function Header() {
+  return (
+    <header>
+      <Title title="Les jobs" />
+      <Button title="Ajouter une offre" />
+    </header>
+  )
+}
+
+export default Header;
